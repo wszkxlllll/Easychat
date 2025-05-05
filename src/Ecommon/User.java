404223e87140186f93e1b1ad19494d1783d24f27
wszkxlllll <2746619732@qq.com>
2025-05-05 21:44:38 +0800

    服务端

---
+++ src/Ecommon/User.java
@@ -0,0 +1,34 @@
+package Ecommon;
+
+import java.io.Serializable;
+/*
+ *表示一个用户信息
+ */
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private String userId;
+    private String passwd;
+    public User() {}
+    public User(String userId, String passwd) {
+        this.userId = userId;
+        this.passwd = passwd;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getPasswd() {
+        return passwd;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public void setPasswd(String passwd) {
+        this.passwd = passwd;
+    }
+
+
+}
